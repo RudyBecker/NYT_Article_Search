ce28e9b58f6da955f8750e2d08601d05630f9752
learn <learn@LEARN-16.local>
2020-03-13 13:58:35 -0700

    end of class commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Jumbotron, Container, InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      search: "",
+      articles: [],
+      pubDate: ""
+    }
+  }
+  search=()=> {
+    this.getArticles(this.state.search)
+  }
+
+  handleChange=(e)=> {
+    this.setState({search: e.target.value})
+  }
+  //
+  // convertDate=(article)=> {
+  //   let date = article;
+  //   let newDate = Date(document.data.date).toString();
+  //   this.setState({pubDate: newDate})
+  // }
+
+  getArticles=(query)=> {
+    let apiKey = process.env.REACT_APP_NYT_KEY;
+    let searchURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}=election&api-key=${apiKey}`;
+
+    fetch(searchURL)
+      .then((response) => {
+        return response.json()
+      })
+      .then((payload) => {
+        let articles = payload.response.docs;
+        console.log(articles)
+        this.setState({articles: articles})
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <Jumbotron fluid>
+         <Container fluid>
+           <h1 className="display-3">New York Times Article Search</h1>
+           <p className="lead">This is an educational API search project. Made by Meo & Rudy.</p>
+         </Container>
+       </Jumbotron>
+
+       <InputGroup>
+        <Input
+          onChange={this.handleChange}
+          type="text"
+          value={this.state.search}
+          placeholder="What articles would you like to see?"
+        />
+        <InputGroupAddon addonType="append">
+          <Button onClick={this.search} color="secondary">Search</Button>
+        </InputGroupAddon>
+      </InputGroup>
+
+      <div>
+        {this.state.articles.map((article, index) => {
+          return (
+            <div key={index}>
+              <a href={article.web_url} target="_blank">{index + 1}: {article.headline.main}</a>
+              <p>{article.pub_date}</p>
+            </div>
+          )
+        })}
+      </div>
+     </>
+    )
+  }
+
 }
 
 export default App;
